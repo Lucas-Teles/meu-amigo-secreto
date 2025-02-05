8cfb4dfe6b69289851d54e2861e64d0051667a0e
Lucas Teles <lucasouzateles@gmail.com>
2025-02-05 19:14:17 -0300

    feat: implementando a lógica e suas validações

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 const nomesDosAmigos = [];
 
-function adicionarAmigos(nomeDoAmigo){
-    nomesDosAmigos.push(nomeDoAmigo);
+function adicionarAmigo(){
+    let amigo = document.getElementById("amigo").value.trim();
+    if(amigo === ""){
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    for (let i = 0; i < nomesDosAmigos.length; i++) {
+      if(nomesDosAmigos[i] == amigo){
+        alert("Esse nome já foi adicionado!");
+        document.getElementById("amigo").value = "";
+        return;
+      }
+    }
+
+    nomesDosAmigos.push(amigo);
+    console.log(nomesDosAmigos);
+
+    document.getElementById("amigo").value = "";
+
 }
